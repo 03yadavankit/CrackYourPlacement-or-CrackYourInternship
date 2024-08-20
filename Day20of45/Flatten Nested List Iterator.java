01bf7fe02ef98e166289a54e7a2006dbd33f8167
03yadavankit <03yadavankit@gmail.com>
2024-08-20 20:18:46 +0530

    Day20of45

---
+++ Day20of45/Flatten Nested List Iterator.java
@@ -0,0 +1,33 @@
+package Day20of45;
+import java.util.*;
+public class NestedIterator implements Iterator<Integer> {
+    private Stack<NestedInteger> stack;
+    
+    public NestedIterator(List<NestedInteger> nestedList) {
+        stack = new Stack<>();
+        for (int i = nestedList.size() - 1; i >= 0; i--) {
+            stack.push(nestedList.get(i));
+        }        
+    }
+
+    @Override
+    public Integer next() {
+        return stack.pop().getInteger();        
+    }
+
+    @Override
+    public boolean hasNext() {
+        while (!stack.isEmpty()) {
+            NestedInteger current = stack.peek();
+            if (current.isInteger()) {
+                return true;
+            }
+            stack.pop();
+            List<NestedInteger> nestedList = current.getList();
+            for (int i = nestedList.size() - 1; i >= 0; i--) {
+                stack.push(nestedList.get(i));
+            }
+        }
+        return false;        
+    }
+}
---
+++ Day20of45/Invert Binary Tree.java
@@ -0,0 +1,29 @@
+package Day20of45;
+import java.util.*;
+class Solution {
+
+    private void invertTreeHelper(TreeNode root)
+    {
+        if (root == null) {
+           return;
+        }
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        if(root.left!=null){
+           invertTree(root.left);
+        }
+        if(root.right!=null){
+          invertTree(root.right);
+        }
+    
+        
+    }
+
+    public TreeNode invertTree(TreeNode root) {
+        
+            invertTreeHelper(root);
+
+            return root;
+    }
+}
