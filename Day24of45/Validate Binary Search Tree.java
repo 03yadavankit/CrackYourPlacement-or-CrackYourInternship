c8bd12f5480c1d54f2f5938f3cf3e616d0378c48
03yadavankit <03yadavankit@gmail.com>
2024-08-24 20:30:16 +0530

    Day24of45

---
+++ Day24of45/Validate Binary Search Tree.java
@@ -0,0 +1,20 @@
+package Day24of45;
+
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        if(root == null) return list;
+        Stack<TreeNode> stack = new Stack<>();
+        while(root != null || !stack.empty()){
+            while(root != null){
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            list.add(root.val);
+            root = root.right;
+            
+        }
+        return list;
+    }
+}
