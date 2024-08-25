e881cfef2f23c976769cfd58f569f18b116b3876
03yadavankit <03yadavankit@gmail.com>
2024-08-25 20:50:37 +0530

    Day25of45

---
+++ Day25of45/Populating next right pointer in each node.java
@@ -0,0 +1,29 @@
+package Day25of45;
+
+class Solution {
+    public Node connect(Node root) {
+        Queue<Node> queue=new LinkedList<>();
+        queue.add(root);
+        bfs(queue);
+        return root;
+    }
+    public void bfs(Queue<Node> queue){
+        Queue<Node> temp=new LinkedList<>();
+        while(!queue.isEmpty()){
+            Node curr=queue.poll();
+            if(curr==null){
+                continue;
+            }
+            if(curr.left!=null){
+                temp.add(curr.left);
+            }
+            if(curr.right!=null){
+                temp.add(curr.right);
+            }
+            curr.next=queue.peek();
+        }
+        if(!temp.isEmpty()){
+            bfs(temp);
+        }
+    }
+}
